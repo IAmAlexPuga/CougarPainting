759d90877428744306e7cf990edcd2e36dcd3e10
Alex Puga <pugapower@gmail.com>
2023-10-24 12:33:35 -0700

    updated proper slides for each section and img content

--- public/src/scripts.js
+++ public/src/scripts.js
@@ -4,7 +4,7 @@ window.addEventListener("resize", positionContent);
 //global vars
 let slideIndex = [1,1,1,1,1,1,1,1,1,1,1];
 let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4",
- "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9"];
+ "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11"];
 
 
 function positionContent() {
